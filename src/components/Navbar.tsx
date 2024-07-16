41ff16fb89f571c94868edc952fb3e3c3204ad80
Rahul Karadwal <rahulkaradwal2000@gmail.com>
2024-07-15 20:03:58 -0400

    feat: add media query and action button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,10 @@
-// import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid"
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid"
 import Logo from "../assets/Logo.png"
+import useMediaQuery from "../hooks/useMediaQuery"
 import NavItem from "./NavItem"
 import { SelectedPage } from "./shared/types"
+import { useState } from "react"
+import ActionButton from "./shared/ActionButton"
 
 type Props = {
     selectedPage: SelectedPage
@@ -10,13 +13,20 @@ type Props = {
 }
 
 
+
+
 const Navbar = ({selectedPage, setSelectedPage}: Props) => {
+
+    const [isMenuToggled, setIsMenuToggled] = useState(false);
+
     const flexBetween = "flex items-center justify-between"
+   const  isAboveMediumScreens = useMediaQuery('( min-width: 1060px )');
   return (
     <nav className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
     <div className={`${flexBetween} mx-auto w-5/6 gap-16` }>
     <img src={Logo} alt="logo" />
-    <div className={`${flexBetween} w-full`}>
+{isAboveMediumScreens ? (
+        <div className={`${flexBetween} w-full`}>
     <div className={`${flexBetween} gap-8 text-sm`}>
 
     <NavItem page="Home" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
@@ -27,11 +37,17 @@ const Navbar = ({selectedPage, setSelectedPage}: Props) => {
     </div>
     <div className={`${flexBetween} gap-8`}>
     <p>Sign In</p>
-    <button>Become a Member</button>
+    <ActionButton setSelectedPage={setSelectedPage}  >Become a Member</ActionButton>
 </div>
 
   
 </div>
+) : (
+    <button className="rounded-full bg-secondary-500 p-2" onClick={()=> setIsMenuToggled(!isMenuToggled)} >
+        <Bars3Icon className="h-6 w-6 text-white" />
+    </button>
+
+)}
 
 </div>
 
---
+++ src/components/shared/ActionButton.tsx
@@ -0,0 +1,15 @@
+import AnchorLink from "react-anchor-link-smooth-scroll"
+import { SelectedPage } from "./types"
+
+type Props = {
+    setSelectedPage : (value: SelectedPage) => void
+    children: React.ReactNode
+}
+
+function ActionButton({setSelectedPage, children}: Props) {
+  return (
+    <AnchorLink className="rounded-md bg-secondary-500 py-2 px-6 text-sm hover:bg-primary-500 transition duration-500" onClick={()=>setSelectedPage(SelectedPage.Contactus)} href="#contactus">{children}</AnchorLink>
+  )
+}
+
+export default ActionButton
